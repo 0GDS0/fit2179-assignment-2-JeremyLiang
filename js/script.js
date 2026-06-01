1f675173ba2f0db669320d821a4b327497ca809a
ChengJia Liang <148967614+0GDS0@users.noreply.github.com>
2026-06-01 23:30:31 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Map 1: Public Transport Density
+var vg_1 = "specs/map_density.json";
+vegaEmbed("#vis-map-density", vg_1, {"actions": true})
+
+// Map 2: Population Density
+var vg_2 = "specs/map_population_final.json";
+vegaEmbed("#vis-map-population", vg_2, {"actions": true})
+
+// Chart 3: Waffle Chart
+var vg_3 = "specs/chart_waffle.json";
+vegaEmbed("#vis-chart-waffle", vg_3, {"actions": true})
+
+// Chart 4: Commuter Profile Stacked Bar
+var vg_4 = "specs/chart_commuter_profile.json";
+vegaEmbed("#vis-commuter-profile", vg_4, {"actions": true})
+
+// Chart 5: Transit Profile Radar Chart
+var vg_5 = "specs/chart_radar.json";
+vegaEmbed("#vis-chart-radar", vg_5, {"actions": true})
+
+// Chart 6: Age Demographics Stacked Bar
+var vg_6 = "specs/chart_age_demographics.json";
+vegaEmbed("#vis-age-demographics", vg_6, {"actions": true})
+
+// Chart 7: Accessibility Gap (Marginal Scatter Plot)
+var vg_7 = "specs/chart_accessibility_gap.json";
+vegaEmbed("#vis-accessibility-gap", vg_7, {"actions": true})
+
+// Chart 8: Passenger Volume Line Chart
+var vg_8 = "specs/chart_passenger_volume.json";
+vegaEmbed("#vis-passenger-volume", vg_8, {"actions": true})
+
+//chart 9: Commute Times Box Plot
+var vg_9 = "specs/chart_commute_times.json";
+vegaEmbed("#vis-commute-times", vg_9, {"actions": true})
+
+//chart 10: Total Utilization Bar Chart
+var vg_10 = "specs/chart_total_utilization.json";
+vegaEmbed("#vis-total-utilization", vg_10, {"actions": true})
+
+// Chart 11: Net Commuter Flow 
+var vg_11 = "specs/chart_commuter_flow.json";
+vegaEmbed("#vis-commuter-flow", vg_11, {"actions": true})
+
+// Chart 12: Mode Shift Bump Chart (Conclusion)
+var vg_12 = "specs/chart_mode_shift_bump.json";
+vegaEmbed("#vis-bump-chart", vg_12, {"actions": true})
